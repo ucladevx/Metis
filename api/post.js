9e98e20f09e544628049f76ec0eeb99a7c5a1f24
legitmaxwu <chengmaxwu@gmail.com>
2019-02-20 20:05:33 -0800

    added prereq post request

--- api/post.js
+++ api/post.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 var router = express.Router();
 
 // functions called by API endpoints
 const helpers = require('../helpers/functions');
+
+require('../schemas/PreRequisites.js');
+const PreRequisitesSchema = mongoose.model('PreRequisites');
 
 router.post('/test/', (req,res)=> {
   res.send("Welcome, " + req.body.name);
@@ -10,6 +14,18 @@ router.post('/test/', (req,res)=> {
 
 router.post('/testjson', (req,res)=> {
   res.send(req.body.name + " " + req.body.age);
+});
+
+router.post('/testModel', function(req, res, next) {
+  for (var i = 0; i < req.body.list.length; i++) {
+    var obj = req.body.list[i];
+    var p = new PreRequisitesSchema({
+      class_name: obj.name,
+      class_prerequisites: obj.prereqs,
+      class_corequisites: obj.coreqs,
+    })
+    p.save();
+  }
 });
 
 module.exports = router;
