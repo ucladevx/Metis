a03ce9e9df69fc6709e683b9e212e43734832093
legitmaxwu <chengmaxwu@gmail.com>
2019-03-13 13:59:03 -0700

    wrote function to find recommended classes given classes taken

--- helpers/functions.js
+++ helpers/functions.js
@@ -1,6 +1,56 @@
+var clusters = require('../python/clustering/clusters.json');
+var classStringMap = {
+  "COM SCI" : "Computer Science",
+  "PHYS" : "Physics",
+  "MATH" : "Mathematics",
+  "KOREA" : "Korean"
+};
 
 function hello(){
   return "Hello";
 }
 
-module.exports = {hello}
+function findRecommended(classesTaken){
+  var recommendations = [];
+  for (var i = 0; i < classesTaken.classes.length; i++)
+  {
+    for (var j = 0; j < classesTaken.classes[i].length; j++)
+    {
+      var rec = new Object();
+      rec.class_taken = classesTaken.classes[i][j];
+      var deptRecs = clusters[classStringMap[rec.class_taken.dept]] 
+      var found = false;
+      for (var k = 0; !found && deptRecs["lowerdiv"] && k < deptRecs["lowerdiv"].length; k++)
+      {
+        var cluster = deptRecs["lowerdiv"][k];
+        if (cluster.includes(rec.class_taken.name))
+        {
+          rec.similar_classes = cluster;
+          found = true;
+        }
+      }
+      for (var k = 0; !found && deptRecs["upperdiv"] && k < deptRecs["upperdiv"].length; k++)
+      {
+        var cluster = deptRecs["upperdiv"][k];
+        if (cluster.includes(rec.class_taken.name))
+        {
+          rec.similar_classes = cluster;
+          found = true;
+        }
+      }
+      for (var k = 0; !found && deptRecs["grad"] && k < deptRecs["grad"].length; k++)
+      {
+        var cluster = deptRecs["grad"][k];
+        if (cluster.includes(rec.class_taken.name))
+        {
+          rec.similar_classes = cluster;
+          found = true;
+        }
+      }
+      recommendations.push(rec);
+    }
+  }
+  return recommendations;
+}
+
+module.exports = {hello, findRecommended}
