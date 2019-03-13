314f79c9563e4b58fa9715413dd64ad198226066
legitmaxwu <chengmaxwu@gmail.com>
2019-03-13 12:49:46 -0700

    -

--- helpers/functions.js
+++ helpers/functions.js
@@ -3,4 +3,18 @@ function hello(){
   return "Hello";
 }
 
+function findRecommended(classesTaken){
+  var recommendations = [];
+  for (var i = 0; i < classesTaken.length; i++)
+  {
+    for (var j = 0; j < classesTaken[i].length; i++)
+    {
+      var obj;
+      obj.classTaken = classesTaken[i][j];
+    }
+  }
+
+  return recommendations;
+}
+
 module.exports = {hello}
