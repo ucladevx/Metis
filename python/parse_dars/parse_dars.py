f91738bec6cd944ad732aa90a220ba8b8218c9f0
legitmaxwu <chengmaxwu@gmail.com>
2019-05-07 21:34:50 -0700

    refactored json structure

--- python/parse_dars/parse_dars.py
+++ python/parse_dars/parse_dars.py
@@ -1,3 +1,9 @@
+# TODO
+#   1) Capture nested AND/OR logic (i.e. 'MATHEMATICS 31A, AND 31B OR 31E')
+#   2) Capture 'Set / Course' logic (see 'SCHOOL | COLLEGE FOREIGN LANGUAGE REQUIREMENT' in DARS)
+#   3) determine what the key titles should be (currently everything is preserved)
+#   4) Possibly capture data from bottom sections (they have a different structure)
+
 from bs4 import BeautifulSoup
 import re
 import json
@@ -9,9 +15,6 @@ reqs = soup.find_all('div', attrs={'class': requirementStatus_})
 
 major_reqs = {}
 
-# remove br
-
-
 for req in reqs:
 
     # get req title!
@@ -21,19 +24,20 @@ for req in reqs:
     #title = ' '.join(title.split())
     
     major_reqs[str(title)] = {}
-    major_reqs[str(title)]['CLASSES'] = []
-    major_reqs[str(title)]['SUBREQUIREMENTS'] = []
+
+    #major_reqs[str(title)]['SUBREQUIREMENTS'] = []
         
     # parse SELECT FROM:
     classes = req.find_all('span', attrs={'class': 'course draggable'})
 
-    for c in classes:
-        # print(c['department'], c['number'])
-        cl = [c['department'], c['number']]
-        major_reqs[str(title)]['CLASSES'].append(cl)
+    # FOR FINDING ALL CLASSES FOR A SUBSECTION
+    # major_reqs[str(title)]['CLASSES'] = []
+    # for c in classes:
+    #     # print(c['department'], c['number'])
+    #     cl = [c['department'], c['number']]
+    #     major_reqs[str(title)]['CLASSES'].append(cl)
     
     # parse ELECTIVE stuff
-    subreqBodies = []
     sections = req.find_all('div', attrs={'class': 'subreqBody'})
     for s in sections:
         subreqBody = {}
@@ -41,17 +45,42 @@ for req in reqs:
         if len(reqTitle.contents) == 0:
             continue
         reqTitle = reqTitle.contents[0]
-        subreqBody['TITLE'] = title
+        
+        # NEEDS
+        subreqBody['NEEDS'] = {}
+        needs = s.find_all('table', attrs={'class': 'subreqNeeds'})
+        for n in needs:
+            #print(n)
+            count = n.find_all('td', attrs={'class': 'count number'})
+            label = n.find_all('td', attrs={'class': 'countlabel fieldlabel'})
+            if count:
+                subreqBody['NEEDS']['COUNT'] = count[0].string
+            if label:
+                subreqBody['NEEDS']['LABEL'] = label[0].string
+                
+        # FROM
+        subreqBody['FROM'] = []
+        From = s.find_all('tr', attrs={'class': 'selectfromcourses'})
+        for f in From:
+            classes = f.find_all('span', attrs={'class': 'course'})
+            for c in classes:
+                cl = {}
+                cl['DEPT'] = c['department']
+                cl['NUM'] = c['number']
+                subreqBody['FROM'].append(cl)
+
+        # NOT FROM
         subreqBody['NOT_FROM'] = []
         notFrom = s.find_all('tr', attrs={'class': 'notfromcourses'})
         for nf in notFrom:
             classes = nf.find_all('span', attrs={'class': 'course'})
             for c in classes:
-                cl = [c['department'], c['number']]
+                cl = {}
+                cl['DEPT'] = c['department']
+                cl['NUM'] = c['number']
                 subreqBody['NOT_FROM'].append(cl)
-        subreqBodies.append(subreqBody)
-    major_reqs[str(title)]['SUBREQUIREMENTS'] = subreqBodies
-    print(subreqBodies)
+        major_reqs[str(title)][reqTitle] = subreqBody
+    
 
         
 
