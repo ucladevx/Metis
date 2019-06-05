a9b71312b009e70e0c1236d0bffb5602e2ce4bdd
Taasin <taasin.saquib@gmail.com>
2019-06-05 11:40:16 -0700

    validMajorCLasses now POST

--- api/post.js
+++ api/post.js
@@ -103,6 +103,7 @@ router.post('/find-recommended', function(req, res, next) {
 //   });
 // });
 
+// Given a department(full name), return tiles to display on frontend
 /* Route parameter:
 {
 	"department": "Computer Science"
@@ -126,7 +127,6 @@ router.post('/initDeptTiles', async function(req,res,next){
 	for(var course of objectArray){
 		courseList.push(course["class_id"]);
 	}
-	//var returnObject = {"department": courseList};
 
 	var returnObject = convertHelpers.convertFormat(courseList);
 	console.log(returnObject);
@@ -175,6 +175,184 @@ router.post('/user', function(req, res, next){
      print(e);
      res.send("Error inserting User document")
   }
+});
+
+// Given department and classes a user has taken, return recommended classes
+/* Route Paramter
+{
+   "department":"Computer Science",
+   "takenCourses":{
+      "classes":{
+         "c1":{
+            "id":"c1",
+            "dept":"COM SCI",
+            "name":"1"
+         },
+         "c2":{
+            "id":"c2",
+            "dept":"COM SCI",
+            "name":"31"
+         },
+         "c3":{
+            "id":"c3",
+            "dept":"ENGCOMP",
+            "name":"3"
+         },
+         "c4":{
+            "id":"c4",
+            "dept":"MATH",
+            "name":"31A"
+         },
+         "c5":{
+            "id":"c5",
+            "dept":"PHYSICS",
+            "name":"1B"
+         },
+         "c6":{
+            "id":"c6",
+            "dept":"PHYSICS",
+            "name":"1C"
+         }
+      },
+      "quarters":{
+         "search":{
+            "id":"search",
+            "title":"To Do",
+            "classIds":[
+               "c5",
+               "c6"
+            ]
+         },
+         "q1":{
+            "id":"q1",
+            "title":"To Do",
+            "classIds":[
+               "c1",
+               "c2"
+            ]
+         },
+         "q2":{
+            "id":"q2",
+            "title":"To Do",
+            "classIds":[
+               "c4"
+            ]
+         },
+         "q3":{
+            "id":"q3",
+            "title":"To Do",
+            "classIds":[
+
+            ]
+         },
+         "q4":{
+            "id":"q4",
+            "title":"To Do",
+            "classIds":[
+               "c3"
+            ]
+         }
+      },
+      "quarterOrder":[
+         "q1",
+         "q2",
+         "q3",
+         "q4"
+      ]
+   }
+}
+*/
+
+//expects "department" and "takenCourses" in req.body
+router.post('/validMajorClasses', async function(req,res,next){
+
+	const dbase = dbHelpers.getDb();
+	const db = dbase.db("Metis");
+	const courses = db.collection("Course");
+	var validClasses = {};
+
+
+	var major = req.body.department;
+	var takenCourses = helperFunctions.convertNames(req.body.takenCourses);
+	try{
+		var objectArray = await courses.find({"department":major}).toArray();
+	} catch(error){
+		console.log(error);
+		res.send(error);
+	}
+
+	var output = {};
+	output.requirementsLeft = requisiteHelpers.validClasses(takenCourses,objectArray);
+
+	let canTake = [];
+	for (cls in output.requirementsLeft) {
+		console.log(cls);
+		if (output.requirementsLeft[cls].length === 0)
+			canTake.push(cls);
+	}
+	output.canTake = canTake;
+
+	let canTakeUpdated = []
+	for (cls in canTake) {
+		cls = canTake[cls];
+		let id = cls.split(" ");
+		id = id[id.length - 1];
+		id = id.replace(/[A-Z]/g, '');
+		id = parseInt(id);
+		if (id < 200)
+			canTakeUpdated.push(cls);
+	}
+
+	var filteredClasses = canTakeUpdated;
+	var recommendedClasses = [];
+	var newTakenCourses = [];
+	for (cls in takenCourses) {
+		cls = takenCourses[cls];
+		let lastIndex = cls.lastIndexOf(" ");
+		let dept = cls.substring(0, lastIndex);
+
+		let deptConverted = reverseMap[dept];
+		let id = cls.split(" ");
+		id = id[id.length - 1];
+		let finalName = deptConverted + " " + id;
+
+		newTakenCourses.push(finalName);
+	}
+	console.log(newTakenCourses)
+	console.log(filteredClasses)
+
+	var abbrevDept = newTakenCourses[0].split(" ");
+	abbrevDept = abbrevDept.splice(0,abbrevDept.length - 1).join(" ")
+	console.log(abbrevDept)
+	for(var course of newTakenCourses){
+
+		try{
+			var cluster = [];
+			for(object of objectArray){
+				if (object["class_id"]==course){
+					cluster = object["similar_classes"];
+					console.log(cluster);
+				}
+
+			}
+			for (var clusterCourseNum of cluster){
+				var clusterCourseName = abbrevDept + " " + clusterCourseNum;
+
+				if (filteredClasses.includes(clusterCourseName) && !newTakenCourses.includes(clusterCourseName))
+					recommendedClasses.push(clusterCourseName);
+			}
+		} catch (e){
+			console.log(e);
+		}
+
+	}
+	output.recommended = recommendedClasses;
+	var returnObject = convertHelpers.convertFormat(output.recommended);
+	//console.log(output);
+	//res.status(200).json(output);
+	res.send(returnObject);
+	return;
+
 });
 
 module.exports = router;
